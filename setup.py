0b97372658726507cfe8e6e649f6a8b42f67ef19
Abhishek Kori <abhishekkori601@gmail.com>
2025-05-07 14:41:07 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="GenAI-MCQ-Generator",          
+    version="0.0.1",
+    author="Akorikk",
+    author_email="abhishekkori601@gmail.com",
+    description="A GenAI-based MCQ generator",
+    packages=find_packages(),            
+    install_requires=[
+        "openai",
+        "langchain",
+        "langchain-community",
+        "pandas",
+        "PyPDF2",
+        "streamlit"
+    ],
+    include_package_data=True,
+)
+
+
